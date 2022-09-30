4627a9a699078a1c5f6b8f184061ecf450382dcd
Ura Yukimitsu <ura@yukimitsu.moe>
2022-09-30 04:35:52 +0200

    Fix command permissions & code formatting

--- command-launch.js
+++ command-launch.js
@@ -4,49 +4,49 @@ async function gen(){
   const { ContextMenuCommandBuilder } = require('@discordjs/builders');
   const { REST } = require('@discordjs/rest');
   const { Routes } = require('discord-api-types/v9');
-	let clientId="972976110038056980";
+	let clientId=process.env["client_id"];
   
   const commands = [
   	new SlashCommandBuilder().setName('ping').setDescription("Bot latency"),
   	new SlashCommandBuilder().setName('64').setDescription("OOT64 Invite"),
   	new SlashCommandBuilder().setName('cows').setDescription("Link to a list of cow locations"),
   	new SlashCommandBuilder().setName('citra').setDescription("Citra info"),
-		new SlashCommandBuilder().setName('dump').setDescription("Dumping guide"),
-		new SlashCommandBuilder().setName('glossary').setDescription("Link to a list of acronyms"),
-		new SlashCommandBuilder().setName('grottos').setDescription("List of grottos"),
-		new SlashCommandBuilder().setName('hash').setDescription("Explains seed hashes"),
-		new SlashCommandBuilder().setName('latest').setDescription("Get latest releases"),
-		new SlashCommandBuilder().setName('logic').setDescription("Explains logic"),
-		new SlashCommandBuilder().setName('letter').setDescription("Explains stuff about Zelda's Letter"),
-		new SlashCommandBuilder().setName('log').setDescription("Spoiler Log info"),
-		new SlashCommandBuilder().setName('multiworld').setDescription("Multiworld info"),
-		new SlashCommandBuilder().setName('progressive').setDescription("Explains progressive items"),
-		new SlashCommandBuilder().setName('rom').setDescription("How to get a ROM"),
-		new SlashCommandBuilder().setName('scrub').setDescription("List of scub locations"),
-		new SlashCommandBuilder().setName('source').setDescription("OOT3DR Source Code"),
-		new SlashCommandBuilder().setName('texture').setDescription("Texture Pack info"),
-		new SlashCommandBuilder().setName('tracker').setDescription("Explains trackers"),
-		new SlashCommandBuilder().setName('update').setDescription("Admin only"),
-		new SlashCommandBuilder().setName('pre').setDescription("Admin only"),
-		new SlashCommandBuilder().setName('commands').setDescription("Help Menu"),
-		new SlashCommandBuilder().setName('help').setDescription("Help Menu"),/*
-		new SlashCommandBuilder().setName('translate').setDescription("Translate")
-		.addStringOption(option =>
-		option.setName('content')
-			.setDescription('What to translate')
-			.setRequired(true))
-		.addStringOption(option =>
-		option.setName('languageFrom')
-			.setDescription("The language it's in (can autodetect)")
-			.setRequired(false)).addStringOption(option =>
-		option.setName('languageTo')
-			.setDescription('The language it needs to be (default: English)')
-			.setRequired(false)),
-		new SlashCommandBuilder().setName('define').setDescription("Define")
-		.addStringOption(option =>
-		option.setName('word')
-			.setDescription('Word to define')
-			.setRequired(true)),*/
+	new SlashCommandBuilder().setName('dump').setDescription("Dumping guide"),
+	new SlashCommandBuilder().setName('glossary').setDescription("Link to a list of acronyms"),
+	new SlashCommandBuilder().setName('grottos').setDescription("List of grottos"),
+	new SlashCommandBuilder().setName('hash').setDescription("Explains seed hashes"),
+	new SlashCommandBuilder().setName('latest').setDescription("Get latest releases"),
+	new SlashCommandBuilder().setName('logic').setDescription("Explains logic"),
+	new SlashCommandBuilder().setName('letter').setDescription("Explains stuff about Zelda's Letter"),
+	new SlashCommandBuilder().setName('log').setDescription("Spoiler Log info"),
+	new SlashCommandBuilder().setName('multiworld').setDescription("Multiworld info"),
+	new SlashCommandBuilder().setName('progressive').setDescription("Explains progressive items"),
+	new SlashCommandBuilder().setName('rom').setDescription("How to get a ROM"),
+	new SlashCommandBuilder().setName('scrub').setDescription("List of scrub locations"),
+	new SlashCommandBuilder().setName('source').setDescription("OOT3DR Source Code"),
+	new SlashCommandBuilder().setName('texture').setDescription("Texture Pack info"),
+	new SlashCommandBuilder().setName('tracker').setDescription("Explains trackers"),
+	new SlashCommandBuilder().setName('update').setDescription("Admin only").setDefaultMemberPermissions('MANAGE_MESSAGES'),
+	new SlashCommandBuilder().setName('pre').setDescription("Admin only").setDefaultMemberPermissions('MANAGE_MESSAGES'),
+	new SlashCommandBuilder().setName('commands').setDescription("Help Menu"),
+	new SlashCommandBuilder().setName('help').setDescription("Help Menu"),/*
+	new SlashCommandBuilder().setName('translate').setDescription("Translate")
+	.addStringOption(option =>
+	option.setName('content')
+		.setDescription('What to translate')
+		.setRequired(true))
+	.addStringOption(option =>
+	option.setName('languageFrom')
+		.setDescription("The language it's in (can autodetect)")
+		.setRequired(false)).addStringOption(option =>
+	option.setName('languageTo')
+		.setDescription('The language it needs to be (default: English)')
+		.setRequired(false)),
+	new SlashCommandBuilder().setName('define').setDescription("Define")
+	.addStringOption(option =>
+	option.setName('word')
+		.setDescription('Word to define')
+		.setRequired(true)),*/
   ]
   	.map(command => command.toJSON());
   
